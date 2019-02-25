db9a5a19dafafa4af8099ce76f938d98661f0e8a
William Diebolt <william@Williams-MacBook-Pro.local>
2019-02-25 10:16:11 -0800

    back to bits

---
+++ chapter2/2-06.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int setbits(int x, int p, int n, int y){
+	//rightmost n bits of y
+	int right_most_y = ((1<<n) - 1) & y;
+	//shifted p positions
+	right_most_y = right_most_y << p;
+
+	//high-end of x
+	int high_x = x >> (n+p-1);
+	high_x = high_x << (n+p-1);
+
+	//low-end of x
+	int low_x = x & ((1<<(p))-1);
+
+	return right_most_y | high_x | low_x;
+}
+
+int main(int argc, char ** argv){
+	int x = 15; 
+	int y = 0;
+	int n = 3; //WORKS if pretend n was n-1
+	int p = 1;
+	printf("x is %d\ny is %d\nn is %d\np is %d\n", x, y, n, p);
+	printf("sebits() is %d\n", setbits(x, p, n, y));  	
+
+
+
+
+
+
+}
+
+
