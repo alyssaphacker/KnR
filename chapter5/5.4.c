681ac5668bdd5335136e72fb3aca1a5ebdf54efd
William Diebolt <wmdiebolt@gmail.com>
2019-05-31 23:53:11 -0700

    strend revised

--- chapter5/5.4.c
+++ chapter5/5.4.c
@@ -5,25 +5,20 @@
 int strend(char * s, char * t);
 
 int main(void){
-	char foo[100] = "I have a at least one submarine.";
+	char foo[100] = "I have at least one but probably more than one submarine.";
 	char bar[] = "marine.";
 	printf("strend ret'd %s", strend(foo, bar) ? "true" : "false");
 }
 
 int strend(char * s, char * t){
-	int s_len = 0;
-	int t_len = 0;
-	char * tmp = s;
-	while(*tmp++)
-		s_len++;
-	s_len++;
-	tmp = t;
-	while(*tmp++)
-		t_len++;
-	t_len++;
-	char * ss = s + s_len - t_len;
-	while(*ss++ == *t++) 
+	int i = 0;
+	int j = 0;
+	while(*(s+i++))
+		;	
+	while(*(t+j++))
 		;
-	ss--;
-	return (ss == s + s_len);
+	char * u = s + i - j;
+	while(*u && *u++ == *t++)
+		;
+	return *t? 0 : 1;	
 }
